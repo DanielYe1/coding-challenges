f973d23a7f1acb677af6cd154f22655cf9058e53
Daniel Ye <danielxinxin1@gmail.com>
2022-11-29 23:50:55 -0300

    Adding max non adjacent subset sum

---
+++ src/problems/NonAdjacentMaxSubsetSum.java
@@ -0,0 +1,19 @@
+package problems;
+
+public class NonAdjacentMaxSubsetSum {
+
+
+    static int maxSubsetSum(int[] arr) {
+
+        int[] memo = new int[arr.length + 2];
+
+        memo[1] = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            memo[i + 1] = Math.max(memo[i], Math.max(arr[i] + memo[i - 1], arr[i]));
+        }
+
+        return Math.max(0, memo[arr.length]);
+    }
+
+}
